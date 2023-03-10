833348bdb92510a29f52cc1080e0e4ac5240fdd1
Chris Swan <478926+cpswan@users.noreply.github.com>
2023-03-10 13:05:46 +0000

    feat: Authentication and shared key working

--- lib/atclient.py
+++ lib/atclient.py
@@ -1,17 +1,20 @@
 import gc
 import ujson as json
-import os
 import time
+import ubinascii
 import usocket as socket # type: ignore
 import ussl as ssl # type: ignore
 from aes import aes_decrypt
 from pem_service import get_pem_parameters, get_pem_key, get_pub_parameters
+from third_party import rsa
+from third_party import string
 
 import logging
+#logging.basicConfig(level = logging.INFO)
 log=logging.getLogger(__name__)
 
 def b42_urlsafe_encode(payload):
-        return string.translate(b2a_base64(payload)[:-1].decode('utf-8'),{ ord('+'):'-', ord('/'):'_' })
+        return string.translate(ubinascii.b2a_base64(payload)[:-1].decode('utf-8'),{ ord('+'):'-', ord('/'):'_' })
 
 def pkcs7pad(text):
     return text+chr(16-len(text)%16)*(16-len(text)%16)
@@ -25,34 +28,6 @@ def send_verb(skt, verb):
     parts = response.decode().split('\n') 
     return parts[0], parts[1]
 
-def read_settings():
-    cwd = os.getcwd()
-    localroot=""        
-    if cwd=="/":
-        # Looks like we're running on a devboard with our code in /
-        localroot="/"
-    else:
-        # Running somewhere else, let's go to parent directory
-        cwds=cwd.split("/")
-        for dir in cwds[1:-1]:
-            localroot=localroot+"/"+dir
-        localroot=localroot+"/"
-    with open(localroot + 'settings.json') as f:
-        settings = json.loads(f.read())
-        return settings['ssid'], settings['password'], settings['atSign'].replace('@', ''), settings['privateKey']
-
-def read_key(atSign):
-    cwd = os.getcwd()
-    if cwd=="/":
-        # Looks like we're running on a devboard with our code in /
-        homeroot="/"
-    else:
-        # Running somewhere else, let's try to get the home directory
-        cwds=cwd.split("/")
-        homeroot="/"+cwds[1]+"/"+cwds[2]+"/"
-    with open(homeroot + '.atsign/keys/@' + atSign + '_key.atKeys') as f:
-        info = json.loads(f.read())
-        return info['aesEncryptPrivateKey'], info['aesEncryptPublicKey'], info['aesPkamPrivateKey'], info['aesPkamPublicKey'], info['selfEncryptionKey']
 
 class atException(Exception):
     pass
@@ -73,9 +48,9 @@ class atClient:
             raise atException("Must provide atsign name and atsign pkam key")
         self.atsign = atsign
         self.recipient = recipient
-        pkamKey = get_pem_parameters(get_pem_key(ubinascii.unhexlify(pkam).decode()))
+        pkamKey = get_pem_parameters(get_pem_key(pkam))
         self.pkamkey = rsa.PrivateKey(pkamKey[0], pkamKey[1], pkamKey[2], pkamKey[3], pkamKey[4])
-        privKey = get_pem_parameters(get_pem_key(ubinascii.unhexlify(encryptpriv).decode()))
+        privKey = get_pem_parameters(get_pem_key(encryptpriv))
         self.encryptpriv = rsa.PrivateKey(privKey[0], privKey[1], privKey[2], privKey[3], privKey[4])
         # Take advantage of the fact that the public key parameters are in the private key :)
         self.encryptpub = rsa.PublicKey(privKey[0], privKey[1])
@@ -95,11 +70,11 @@ class atClient:
             rootsock.settimeout(timeout)       # timeout for connect
             rootsock.connect(addr)
             rootsock = ssl.wrap_socket(rootsock, **self.ssl_params)
-            rootsock.do_handshake()
+            #rootsock.do_handshake()
             mb= "{}\n".format(self.atsign).encode("utf-8")
             log.info("sending root request {}", mb)
             rootsock.write(mb)
-            rootsock.settimeout(timeout)       # timeout for read
+            #rootsock.settimeout(timeout)       # timeout for read
             resp = rootsock.readline()
             if resp is None or len(resp)<4:
                 raise atException("timeout")
@@ -125,7 +100,7 @@ class atClient:
             self.sock.settimeout(timeout)       # timeout for connect
             self.sock.connect(addr)
             self.sock = ssl.wrap_socket(self.sock, **self.ssl_params)
-            self.sock.do_handshake()
+            #self.sock.do_handshake()
             log.info("Connected OK to atServer {}:{}, info request...", self.server, self.port)
             response, command = send_verb(self.sock, 'info:brief')
             log.info("atSign info response : {}", response)
@@ -135,15 +110,91 @@ class atClient:
             self.sock = None
             raise err
 
-def main():
+    def authenticate(self):
+        try:
+            log.info("Doing PKAM authentication to {}", self.atsign)
+            response, command = send_verb(self.sock, 'from:' + self.atsign)
+            if response is None or len(response)<4:
+                raise ATException("timeout")
+            challenge = response.replace('@data:', '')
+            log.info("atsign pkam challenge : {}", challenge)
+            signature = b42_urlsafe_encode(rsa.sign(challenge, self.pkamkey, 'SHA-256'))
+            # Key manipulation creates a lot of garbage, so let's clear that up now the signature is ready
+            gc.collect()
+            log.info("atsign pkam signature : {}", signature)
+            response, command = send_verb(self.sock, 'pkam:' + signature)
+            log.info("atsign pkam authenticated : {}", response)
+        except Exception as err:
+            log.exc(err, "during atsign server {}:{} cnx", self.server, self.port)
+            self.sock.close()
+            self.sock = None
+            raise err
 
-    ssid, password, atSign, privateKey = read_settings()
-    print(ssid,password,atSign,privateKey)
-    aesEncryptPrivateKey, aesEncryptPublicKey, aesPkamPrivateKey, aesPkamPublicKey, selfEncryptionKey = read_key(atSign)
-    pkamPrivateKey = aes_decrypt(aesPkamPrivateKey, selfEncryptionKey)
-    encryptPrivateKey = aes_decrypt(aesEncryptPrivateKey, selfEncryptionKey)
-    print(pkamPrivateKey)
-    print(encryptPrivateKey)
+    def getsharedkey(self):
+        try:
+            log.info("Fetching sharedAESKey for {}", self.recipient)
+            response, command = send_verb(self.sock, 'llookup:shared_key.' + self.recipient +'@' + self.atsign)
+            log.info("Got this response for llookup: {}", response)
+            if response is None or len(response)<4:
+                raise ATException("timeout")
+            if response.startswith('@' + self.atsign + '@'):
+                response=response.replace('@' + self.atsign + '@','')
+                log.info("Truncated response is: {}", response)
+            # does my atSign already have the recipient's shared key?
+            if response.startswith('data:'):
+                shared_key = rsa.decrypt(ubinascii.a2b_base64(response.replace('data:','')), self.encryptpriv)
+                self.sharedkey=ubinascii.a2b_base64(shared_key)
+                log.info("Got shared key from atServer: {}", shared_key)
+            # or do I need to create, store and share a new shared key?
+            elif response.startswith('error:AT0015-key not found'):
+                log.info("No local copy of the key, so we need to make one")
+                # Generate an AES256 key and base64 encode it
+                self.sharedkey=ucrypto.getrandbits(256)
+                shared_key = ubinascii.b2a_base64(self.sharedkey).rstrip()
+                log.info("Generated shared key {}", shared_key)
+                # Encrypt the shared_key with our RSA public key
+                encrypted_shared_key = ubinascii.b2a_base64(rsa.encrypt(shared_key, self.encryptpub)).rstrip().decode()
+                log.info("Encrypted shared key {}", encrypted_shared_key)
+                # Store the shared key
+                response, command = send_verb(self.sock, 'update:shared_key.' + self.recipient +'@' + self.atsign + ' ' + encrypted_shared_key)
+                log.info("Got this response for update: {}", response)
+                # Get public key for recipient
+                response, command = send_verb(self.sock, 'plookup:publickey@' + self.recipient)
+                log.info("Got this public key: {}", response)
+                rpubkey=response.replace('@' + self.atsign + '@data:','')
+                log.info("Trimmed public key down to: {}", rpubkey)
+                # Use their public key to encrypt the shared key
+                rxKey = get_pub_parameters(rpubkey)
+                self.receivepub = rsa.PublicKey(rxKey[0], rxKey[1])
+                rencrypted_shared_key = ubinascii.b2a_base64(rsa.encrypt(shared_key, self.receivepub)).rstrip().decode()
+                # Send the shared key
+                response, command = send_verb(self.sock, 'update:ttr:86400:@' + self.recipient + ':shared_key@' + self.atsign + ' ' + rencrypted_shared_key)
+                log.info("Got this response for sharing: {}", response)
+            else:
+                # Something has gone wrong and we don't have a shared key to work with
+                log.warning("No shared key found or created during atsign server {}:{} cnx", self.server, self.port)
+                self.sock.close()
+                self.sock = None
+                raise Exception("Shared key not found or created")
+        except Exception as err:
+            log.exc(err, "during atsign server {}:{} cnx", self.server, self.port)
+            self.sock.close()
+            self.sock = None
+            raise err
 
-if __name__ == '__main__':
-    main()
+    def attalk(self, msg:bytes):
+        try:
+            log.info("publish message to astign: {}",msg)
+            #TODO ivs should not be zeroes, and should be shared in metadata
+            iv = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+            #aes = ucryptolib.aes(self.sharedkey, 6, iv)
+            #b64encrypted_msg = ubinascii.b2a_base64(aes.encrypt(msg)).rstrip().decode()
+            encrypted_msg = ucrypto.AES(self.sharedkey, mode=ucrypto.AES.MODE_CTR, counter=iv).encrypt(pkcs7pad(msg))
+            b64encrypted_msg = ubinascii.b2a_base64(encrypted_msg).rstrip().decode()
+            shared_key = ubinascii.b2a_base64(self.sharedkey).rstrip().decode()
+            response, command = send_verb(self.sock,
+                'notify:update:ttr:-1:@'+self.recipient+':message.infrafon@'+self.atsign+':'+b64encrypted_msg)
+            log.info("Got this response for publishing: {}", response)
+        except Exception as err:
+            log.exc(err, "during info publish to atsign server {}:{} cnx", self.server, self.port)
+            self.disconnect()
--- test.py
+++ test.py
@@ -1,10 +1,17 @@
+import sys
+# Needed when running on Linux to find imports in lib directory
+sys.path.append('./lib')
 import ujson as json
 import os
 from aes import aes_decrypt
+from atclient import atClient
+
+import logging
+logging.basicConfig(level = logging.INFO)
+log=logging.getLogger(__name__)
 
 
 def read_settings():
-    print(os.getcwd())
     with open(os.getcwd() + '/settings.json') as f:
         settings = json.loads(f.read())
         return settings['ssid'], settings['password'], settings['atSign'].replace('@', ''), settings['privateKey']
@@ -23,12 +30,25 @@ def read_key(atSign):
         return info['aesEncryptPrivateKey'], info['aesEncryptPublicKey'], info['aesPkamPrivateKey'], info['aesPkamPublicKey'], info['selfEncryptionKey']
 
 def main():
-
+    atRoot='root.atsign.org'
+    atRecipient='cpswan'
     ssid, password, atSign, privateKey = read_settings()
-    print(ssid,password,atSign,privateKey)
     aesEncryptPrivateKey, aesEncryptPublicKey, aesPkamPrivateKey, aesPkamPublicKey, selfEncryptionKey = read_key(atSign)
     pkamPrivateKey = aes_decrypt(aesPkamPrivateKey, selfEncryptionKey)
-    print(pkamPrivateKey)
+    encryptPrivateKey = aes_decrypt(aesEncryptPrivateKey, selfEncryptionKey)
+
+    if sys.platform != 'linux':
+        wlan = network.WLAN(network.STA_IF)  # type: ignore
+        wlan.active(True)
+        wlan.connect(ssid, password)
+    
+    ssl_params= { 'server_hostname':atRoot}
+    atc = atClient(rootserver=atRoot, atsign=atSign, pkam=pkamPrivateKey,
+        encryptpriv=encryptPrivateKey, recipient=atRecipient, ssl_params=ssl_params)
+    atc.connect()
+    atc.authenticate()
+    atc.getsharedkey()
+    atc.attalk(msg=b'Hello World!')
 
 if __name__ == '__main__':
     main()
